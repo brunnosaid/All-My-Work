60670b7edb5dc384938a5afbebb1502a82232d99
Brunno Said <128349520+brunnosaid@users.noreply.github.com>
2024-07-11 19:24:31 +0000

    Versão inicial

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,20 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+def main():
+    st.title("Redirecionamento para Links")
+    
+    # Criação dos botões para os links
+    if st.button("CyberSecurity"):
+        st.markdown("[TryHackMe](https://tryhackme.com/p/AsherAxe23)")
+        st.markdown("[PicoCTF](https://play.picoctf.org/users/Ash3rAx3)")
+    
+    if st.button("Social Media"):
+        st.markdown("[Instagram](https://www.instagram.com/brunnosaid)")
+        st.markdown("[FaceBook](https://www.facebook.com/brunnosaid)")
+        st.markdown("[TwitchTV](https://www.twitchtv.com/brunnosaid)")
+    
+    if st.button("Donate"):
+        st.markdown("[QR Code](https://github.com)")
+
+if __name__ == "__main__":
+    main()
